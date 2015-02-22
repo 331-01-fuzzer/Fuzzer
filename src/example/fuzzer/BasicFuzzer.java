d0fe71dd771562240f55f64e73eec4f42e5051ca
jonshippling <jjs5471@rit.edu>
2015-02-22 15:31:36 -0500

    implemented flag and file reading

--- src/example/fuzzer/BasicFuzzer.java
+++ src/example/fuzzer/BasicFuzzer.java
@@ -1,26 +1,99 @@
 package example.fuzzer;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.List;
-
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.*;
 
 public class BasicFuzzer {
 
+  /*
+  Possible flags:
+    --custom-auth=string     Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa). Optional.
+
+  Discover options:
+    --common-words=file    Newline-delimited file of common words to be used in page guessing and input guessing. Required.
+
+  Test options:
+    --vectors=file         Newline-delimited file of common exploits to vulnerabilities. Required.
+    --sensitive=file       Newline-delimited file data that should never be leaked. It's assumed that this data is in the application's database (e.g. test data), but is not reported in any response. Required.
+    --random=[true|false]  When off, try each input to each page systematically.  When on, choose a random page, then a random input field and test all vectors. Default: false.
+    --slow=500             Number of milliseconds considered when a response is considered "slow". Default is 500 milliseconds
+   */
+  static Map<String,String> flags = new HashMap<String,String>();
+  
+  /**
+   *
+   * @param args
+   * @throws FailingHttpStatusCodeException
+   * @throws MalformedURLException
+   * @throws IOException
+   */
+
+  //   fuzz test http://localhost:8080 --custom-auth=dvwa --common-words=words.txt --vectors=vectors.txt --sensitive=creditcards.txt --random=false --slow=500
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		WebClient webClient = new WebClient();
-		webClient.setJavaScriptEnabled(true);
-		discoverLinks(webClient);
-		doFormPost(webClient);
-		webClient.closeAllWindows();
+    String[] loginDetails = new String[2];
+
+    String runType = args[0];
+    String url = args[1];
+    readFlags(Arrays.copyOfRange(args, 2, args.length));
+
+    if(flags.containsKey("--custom-auth")) loginDetails = getAuth(flags.get("--custom-auth"));
+    else {
+      loginDetails[0]="";
+      loginDetails[1]="";
+    }
+
+//
+//		WebClient webClient = new WebClient();
+//		webClient.setJavaScriptEnabled(true);
+//		discoverLinks(webClient);
+//		doFormPost(webClient);
+//		webClient.closeAllWindows();
 	}
+
+  private static void readFlags(String[] args) {
+    for(String s: args) {
+      String[] kv = s.split("=");
+      flags.put(kv[0],kv[1]);
+    }
+  }
+
+  private static String[] getAuth(String customAuth) {
+    String[] loginDetails = new String[2];
+
+    if(customAuth.toLowerCase()=="dvwa") {
+      loginDetails[0]="";
+      loginDetails[1]="";
+    } else if(customAuth.toLowerCase()=="bodgeit") {
+      loginDetails[0]="";
+      loginDetails[1]="";
+    } else {
+      loginDetails[0]="";
+      loginDetails[1]="";
+    }
+
+    return loginDetails;
+  }
+
+  private static List<String> readFile(String fileName) throws IOException {
+    List<String> lines = new ArrayList<String>();
+
+    FileReader in = new FileReader(fileName);
+    BufferedReader br = new BufferedReader(in);
+
+    String line="";
+    while((line = br.readLine()) != null) {
+      lines.add(line);
+    }
+
+    return lines;
+  }
 
 	/**
 	 * This code is for showing how you can get all the links on a given page, and visit a given URL
